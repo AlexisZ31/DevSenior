cf1f68ad249bd933b21d10eeff704f4d738fc8c3
AlexisZ <alexiszara65@gmail.com>
2025-08-05 22:12:27 -0500

    Se eliminaron Archivos innecesarios del proyecto Para crear nuevos

---
+++ 1_vari_tipoData_operadors.py
@@ -0,0 +1,33 @@
+# EJERCICIOS.
+
+# Nº 1
+"""
+    Crea un comentario en el código y coloca la URL 
+    del sitio web oficial del lenguaje de programación que has seleccionado.
+"""
+# Esto es es un comentario de una sola linea, URL oficial python https://www.python.org/
+"""
+    Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).
+"""
+# Comentario de una linea
+"""
+Comentario de
+multiples lineas
+"""
+
+# Crea una variable (y una constante si el lenguaje lo soporta).
+Variable = "Esto es una variable"
+CONTANTE = "Constante todo en  mayuscula"
+
+#  Crea variables representando todos los tipos de datos primitivos
+#  del lenguaje (cadenas de texto, enteros, booleanos...).
+string = "Esto es un estring"
+entero = 7654
+boleano = True
+Flotante = 3.14
+NuComplejo = 4j
+
+# Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("Hola, Python")
+
